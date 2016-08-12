087f143742ff2ff4f3bdfd3033d5b9b2ff694b4a
Tommi Sullivan <studio39.co@gmail.com>
2016-08-12 22:09:25 +1000

    i hate you nate

--- app.js
+++ app.js
@@ -4,6 +4,30 @@ var app = express();
 var http = require('http').Server(app);
 var fs = require('fs');
 var chalk = require('chalk');
+var csv = require('fast-csv');
+var content = '{"data": []}'
+var obj = JSON.parse(content);
+
+/*fs.createReadStream("rent.csv")
+	.pipe(csv())
+	.on("data", function (data) {
+		obj['data'].push({
+			"postCode": data[0].substr(3),
+			"avgInc": data[3],
+			"avgRent": data[4],
+			"avgMorgage": data[2]
+		})
+	})
+	.on("end", function () {
+		console.log("done");
+		content = JSON.stringify(obj);
+		console.log(content);
+		fs.writeFile('data.json', content, (err) => {
+			if (err) throw err;
+			console.log('It\'s saved!');
+		});
+
+	});*/
 
 //server public files
 app.use(express.static(__dirname + '/public'));
