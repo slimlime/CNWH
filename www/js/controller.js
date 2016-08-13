fc0303d32e4a1a5f178d8b399b3e21575ba4a28b
Nate Sanchez-Goodwin <Nate@nate-mac.local>
2016-08-13 10:01:07 +1000

    add GSDK

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,7 +1,8 @@
 app.controller('postcodeCtrl', function($scope, $http) {
     
-    $http.get('/data/1').then(function(response) {
+    $http.get('/data/0').then(function(response) {
         $scope.postcodes = response.data;
+        console.log(response.data);
     });
 
 });
